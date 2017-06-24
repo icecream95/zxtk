01d5be07360e5b55a8b0386cc6e8941223af7d76
icecream95 <icecream95@users.noreply.github.com>
2017-06-24 20:34:37 +1200

    Various stuff

--- src/include/zxtk/modules/z80.hpp
+++ src/include/zxtk/modules/z80.hpp
@@ -1,6 +1,9 @@
 #ifndef ZXTK_MODULES_Z80_INCLUDE_GUARD
 #define ZXTK_MODULES_Z80_INCLUDE_GUARD
 
+#include <zxtk/misc/zxtk_config.hpp>
+#include <zxtk/misc/zxtk_types.hpp>
+
 namespace zxtk
 {
     namespace cpu
@@ -8,7 +11,19 @@ namespace zxtk
         class Z80
         {
         public:
-            
+        protected:
+            struct Default_cpu_impl
+            {
+                inline void nop()
+                {
+#ifndef ZXTK_CORRECT_TIMING
+#else
+                   //advance(4);
+#endif
+                };
+            protected:
+                //void advance(types::
+            };
         };
     }
 }
