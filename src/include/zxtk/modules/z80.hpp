272a763d241c9bcdb56d520382a8fb5ceac5c3af
icecream95 <icecream95@users.noreply.github.com>
2017-07-01 20:41:45 +1200

    Added cycle type

--- src/include/zxtk/modules/z80.hpp
+++ src/include/zxtk/modules/z80.hpp
@@ -15,16 +15,24 @@ namespace zxtk
             struct Default_cpu_impl
             {
                 inline void nop()
-                {
+		    {
 #ifdef ZXTK_Z80_CORRECT_TIMING
 #pragma NOSUCHPRAGMA NOTE: Correct timing feature has not been implemented yet, falling back to incorrect time. This will cause multicolour programs, and some others, to not work correctly
 #endif
-                   advance(4);
-                };
+			advance(4);
+		    };
             protected:
                 void advance(int i)
-                {
-                }
+                    {
+                        // if (diff_cycle(mcycle,cycle)>i)
+                        //     c += i;
+#ifndef MULTIPLE_THREADS_BLAH_BLAH_BLAH
+                        // else
+                        // random stuff
+#endif
+                    }
+                types::cycle cycle {0};
+                // TODO: Add 'fake const' mcycle
             };
         };
     }
