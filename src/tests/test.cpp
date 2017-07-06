000940103be2eea0434a3228e4fabbcedc7d78af
icecream95 <the.real.icecream95@gmail.com>
2017-07-06 18:21:24 +1200

    Remove exception handler - this was not needed

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -2,13 +2,7 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char* argv[])
-try
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}
-catch (...)
-{
-    std::cout << "Exception during testing. Exiting...\n";
-    return 1;
 }
