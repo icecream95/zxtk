a430e3be66ba591d7559b313c3e45941aea52e4b
icecream95 <icecream95@users.noreply.github.com>
2017-07-06 17:19:22 +1200

    Removed useless code to try and make codecov happy

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -3,13 +3,10 @@
 
 int main(int argc, char* argv[])
 try {
-    if (0 == 1)
-        throw 1; // Make codecov happy
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-catch (...)
-{
+catch (...) {
     std::cout << "Exception during testing. Exiting...\n";
     return 1;
 }
