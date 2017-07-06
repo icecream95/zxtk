86d4082b0448b732e4d29b36e9c957e84e81ecd1
icecream95 <icecream95@users.noreply.github.com>
2017-07-06 12:55:42 +1200

    Memory implementation work

--- src/include/zxtk/modules/memory.hpp
+++ src/include/zxtk/modules/memory.hpp
@@ -1,12 +1,33 @@
 #ifndef ZXTK_MODULES_MEMORY_INCLUDE_GUARD
 #define ZXTK_MODULES_MEMORY_INCLUDE_GUARD
 
+#include <array>
+
 #include <zxtk/utilities/buffer.hpp>
 #include <zxtk/misc/zxtk_types.hpp>
 
 namespace zxtk {
     namespace memory {
+        template<typename M = types::pointer, typename B = types::cycle>
+        class Memory_array { // TODO
+        };
+
+        class Memory_array_spectrum { // TODO
+        };
+
+        template<types::pointer S>
+        class Basic_memory_array_test { // TODO: Remove this when Memory_Array is done
+        public:
+            wtite
+        protected:
+            array<types::byte,S>;
+        };
+
+        template<typename A = Memory_array>
         class Memory {
+        public:
+        private:
+            A m;
         }
     }
 }
