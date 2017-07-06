eddd1b468de493bb522b1bfa9c913daeacc3ad88
icecream95 <icecream95@users.noreply.github.com>
2017-07-06 13:01:09 +1200

    Added try block to tests

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,7 +1,12 @@
+#include <iostream>
 #include <gtest/gtest.h>
 
 int main(int argc, char* argv[])
-{
+try {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+}
+catch (...)
+{
+    std::cout << "Exception during testing. Exiting...";
 }
