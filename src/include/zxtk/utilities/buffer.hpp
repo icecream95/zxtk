e801b7f755287b23a2524a6080e826d7e31ce076
Icecream95 <the.real.icecream95@gmail.com>
2017-10-10 12:25:49 +1300

    More work on Buffers

--- src/include/zxtk/utilities/buffer.hpp
+++ src/include/zxtk/utilities/buffer.hpp
@@ -35,12 +35,14 @@ namespace zxtk { // Should this be in here?
             int priority;                                           // Lower priorities will be used first
         };
 
-        template<typename T, std::size_t N, typename C = std::array<T,N>,bool O = true,
+        template<typename T, std::size_t N, typename C = std::array<T,N>, bool O = true,
         typename L = unsigned long long, typename Q = std::priority_queue<Buf_access_info<T,L>>>
+        // We need a deque to store changes that have been added but not yet synced with all the clients
         class Buffer_store {
         public:
-            template<typename U, typename M>
+            template<typename U, std::size_t P, typename D, bool R, typename M, typename S>
             friend class Buffer_client;
+
             using tick_type = L;
             // Note the absence of usual stuff like value_type
             // TODO: Make a class Buffer_timepoint for giving data to
@@ -63,9 +65,8 @@ namespace zxtk { // Should this be in here?
             Q queue; // Q defaults to a priority_queue<Buf_access_info<T,L>>
         };
 
-        template<typename T, typename L> // etc.
-        // Should the priority and read/write permissions be templated?
-        // If so, we should also have a dynamic version
+        template<typename T, std::size_t N, typename C = std::array<T,N>, bool O = true,
+                 typename L = unsigned long long, typename Q = std::priority_queue<Buf_access_info<T,L>>>
         // We should store a cache of the array at the current tick so if
         //     monitoring devices which are slow are attached, the global tick
         //     won't have to wait, just pass the popped entries from the
@@ -74,7 +75,19 @@ namespace zxtk { // Should this be in here?
         //     queue as none of the "slow" clients would have to wait for each
         //     other.
         class Buffer_client {
+        public:
+            Buffer_client(Buffer_store<T, N, C, O, L, Q>);
+            // There will be multiple types of Buffer_store, such as a Buffer_slice and Buffer_multi [possibly with other names],
+            //     so we should allow these. Should a wrapper class or std::variant be used?
         };
+
+
+        template<typename T, std::size_t N, typename C = std::array<T,N>, bool O = true,
+                 typename L = unsigned long long, typename Q = std::priority_queue<Buf_access_info<T,L>>>
+        Buffer_client<T, N, C, O, L, Q> make_buffer(Buffer_store<T, N, C, O, L, Q>& s)
+        {
+            return {s};
+        }
     }
 }
 #endif
