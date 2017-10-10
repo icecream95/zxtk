30f9da679fd2bd841e83979c782d012ce907f4af
Icecream95 <the.real.icecream95@gmail.com>
2017-10-10 16:37:30 +1300

    Clean formatting

--- src/include/zxtk/utilities/exchange.hpp
+++ src/include/zxtk/utilities/exchange.hpp
@@ -6,19 +6,15 @@
 namespace zxtk {
 
 #if _cplusplus < 201402L
-
-template<class T, class U = T>
-T exchange(T& obj, U&& new_value)
-{
-    T old_value = std::move(obj);
-    obj = std::forward<U>(new_value);
-    return old_value;
-}
-
+    template<class T, class U = T>
+    T exchange(T& obj, U&& new_value)
+    {
+        T old_value = std::move(obj);
+        obj = std::forward<U>(new_value);
+        return old_value;
+    }
 #else
-
-using std::exchange;
-
+    using std::exchange;
 #endif
 }
 #endif
